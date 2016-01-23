babeccc65d2a1842bca99243c302cd1636d6a257
Sindri55 <sindri12@ru.is>
2016-01-23 00:22:17 +0000

    check chek

--- app/src/main/java/com/example/swipingapp/MainActivity.java
+++ app/src/main/java/com/example/swipingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.swipingapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tx = (TextView) findViewById(R.id.tx);
+        tx.setTextColor(getResources().getColor(R.color.colorPrimary));
     }
 }
