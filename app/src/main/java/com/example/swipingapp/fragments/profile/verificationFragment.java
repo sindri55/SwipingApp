f46b4ad3e73c2a8ab1a842b8e3890b8b96ab4368
Sindri55 <sindri12@ru.is>
2016-02-12 14:38:04 +0000

    small fix

--- app/src/main/java/com/example/swipingapp/fragments/profile/verificationFragment.java
+++ app/src/main/java/com/example/swipingapp/fragments/profile/verificationFragment.java
@@ -1,7 +1,46 @@
 package com.example.swipingapp.fragments.profile;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.swipingapp.R;
+import com.example.swipingapp.fragments.base.BaseFragment;
+
 /**
  * Created by Sindri on 11/02/16.
  */
-public class verificationFragment {
+public class verificationFragment extends BaseFragment {
+
+    // region Constants
+
+    public static final String TAG = SettingsFragment.class.getSimpleName();
+
+    // endregion
+
+    // region Properties
+
+    // endregion
+
+    // region UI references
+
+    // endregion
+
+    // region Override functions
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.fragment_profile_verification_settings, container, false);
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(R.string.fragment_profile_profile_settings_title);
+    }
+
+    // endregion
 }
