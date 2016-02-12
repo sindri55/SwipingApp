6f387d8c6f2846f1b24522254016a8266c115b2c
hannesg11 <hannesg11@ru.is>
2016-02-12 22:29:10 +0000

    Fixed typo in class name and added the TAG constant

--- app/src/main/java/com/example/swipingapp/fragments/profile/verificationFragment.java
+++ app/src/main/java/com/example/swipingapp/fragments/profile/verificationFragment.java
@@ -1,7 +1,22 @@
 package com.example.swipingapp.fragments.profile;
 
-/**
- * Created by Sindri on 11/02/16.
- */
-public class verificationFragment {
+import com.example.swipingapp.fragments.base.BaseFragment;
+
+public class VerificationFragment extends BaseFragment{
+
+    // region Constants
+
+    public static final String TAG = VerificationFragment.class.getSimpleName();
+
+    // endregion
+
+
+    // region Override functions
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    // endregion
 }
