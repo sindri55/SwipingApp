d3c311e508fe7b8dff8cff4c1b0e6aecb3d451ad
hannesg11 <hannesg11@ru.is>
2016-02-13 00:45:26 +0000

    Added basic client side validation for Login activity

--- app/src/main/java/com/example/swipingapp/activities/account/LoginActivity.java
+++ app/src/main/java/com/example/swipingapp/activities/account/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import com.example.swipingapp.R;
 import com.example.swipingapp.activities.main.MainActivity;
 import com.example.swipingapp.responses.ErrorResponse;
 import com.example.swipingapp.services.account.AccountService;
+import com.example.swipingapp.services.account.AccountServiceStub;
 import com.example.swipingapp.services.account.IAccountService;
 import com.example.swipingapp.utils.DialogUtils;
 import com.example.swipingapp.viewModels.account.LoginViewModel;
@@ -39,8 +41,8 @@ public class LoginActivity extends AppCompatActivity {
 
     // region UI references
 
-    private EditText mEmailView;
-    private EditText mPasswordView;
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
     private Button mLoginButton;
     private TextView mRegisterText;
 
@@ -53,10 +55,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAccountService = AccountService.getInstance();
+        mAccountService = AccountServiceStub.getInstance();
 
-        mEmailView = (EditText) findViewById(R.id.email);
-        mPasswordView = (EditText) findViewById(R.id.password);
+        mEmailInput = (EditText) findViewById(R.id.input_email);
+        mPasswordInput = (EditText) findViewById(R.id.input_password);
         mLoginButton = (Button) findViewById(R.id.btn_login);
         mRegisterText = (TextView) findViewById(R.id.txt_register);
 
@@ -69,6 +71,21 @@ public class LoginActivity extends AppCompatActivity {
     // endregion
 
     // region Private functions
+
+    private boolean validateModel() {
+        boolean valid = true;
+
+        if(TextUtils.isEmpty(mEmailInput.getText())) {
+            mEmailInput.setError(getString(R.string.error_field_required));
+            valid = false;
+        }
+        if(TextUtils.isEmpty(mPasswordInput.getText())) {
+            mPasswordInput.setError(getString(R.string.error_field_required));
+            valid = false;
+        }
+
+        return valid;
+    }
 
     private void updateButtonState(final boolean enabled) {
         mLoginButton.setEnabled(enabled);
@@ -93,15 +110,17 @@ public class LoginActivity extends AppCompatActivity {
                 return;
             }
 
-            updateButtonState(false);
+            if(validateModel()) {
+                updateButtonState(false);
 
-            String email = mEmailView.getText().toString();
-            String password = mPasswordView.getText().toString();
+                String email = mEmailInput.getText().toString();
+                String password = mPasswordInput.getText().toString();
 
-            LoginViewModel loginViewModel = new LoginViewModel(email, password);
+                LoginViewModel loginViewModel = new LoginViewModel(email, password);
 
-            mUserLoginResponse = new UserLoginResponse();
-            mAccountService.login(loginViewModel, mUserLoginResponse);
+                mUserLoginResponse = new UserLoginResponse();
+                mAccountService.login(loginViewModel, mUserLoginResponse);
+            }
         }
     }
 
