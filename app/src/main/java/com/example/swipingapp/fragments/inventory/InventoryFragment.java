4603f7dbc94ac418bbeaf53263ddbe08d15cefb2
hannesg11 <hannesg11@ru.is>
2016-02-16 02:06:31 +0000

    Removed hardcoded values in the list

--- app/src/main/java/com/example/swipingapp/fragments/inventory/InventoryFragment.java
+++ app/src/main/java/com/example/swipingapp/fragments/inventory/InventoryFragment.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.swipingapp.DTOs.inventory.CategoryDTO;
-import com.example.swipingapp.DTOs.inventory.ItemDTO;
 import com.example.swipingapp.R;
 import com.example.swipingapp.adapters.SectionAdapter;
 import com.example.swipingapp.enums.Currency;
@@ -81,55 +80,12 @@ public class InventoryFragment extends BaseFragment {
 
         View view = inflater.inflate(R.layout.fragment_inventory, container, false);
 
-        ItemDTO item1 = new ItemDTO(1, "item1", 2, Currency.ICELANDIC_KRONA);
-        ItemDTO item2 = new ItemDTO(2, "item2", 4, Currency.ICELANDIC_KRONA);
-        ItemDTO item3 = new ItemDTO(3, "item3", 5, Currency.ICELANDIC_KRONA);
-        ItemDTO item4 = new ItemDTO(4, "item4", 20, Currency.ICELANDIC_KRONA);
-
-        ItemDTO item5 = new ItemDTO(1, "item10", 2, Currency.ICELANDIC_KRONA);
-        ItemDTO item6 = new ItemDTO(2, "item20", 4, Currency.ICELANDIC_KRONA);
-        ItemDTO item7 = new ItemDTO(3, "item30", 5, Currency.ICELANDIC_KRONA);
-        ItemDTO item8 = new ItemDTO(4, "item40", 20, Currency.ICELANDIC_KRONA);
-        ItemDTO item9 = new ItemDTO(4, "item50", 20, Currency.ICELANDIC_KRONA);
-
-
-        ItemDTO item10 = new ItemDTO(1, "item100", 2, Currency.ICELANDIC_KRONA);
-        ItemDTO item11 = new ItemDTO(2, "item200", 4, Currency.ICELANDIC_KRONA);
-        ItemDTO item12 = new ItemDTO(3, "item300", 5, Currency.ICELANDIC_KRONA);
-        ItemDTO item13 = new ItemDTO(4, "item400", 20, Currency.ICELANDIC_KRONA);
-        ItemDTO item14 = new ItemDTO(4, "item500", 20, Currency.ICELANDIC_KRONA);
-
-
-        ArrayList<ItemDTO> list1 = new ArrayList<>();
-        list1.add(item1);
-        list1.add(item2);
-        list1.add(item3);
-        list1.add(item4);
-
-        ArrayList<ItemDTO> list2 = new ArrayList<>();
-        list2.add(item5);
-        list2.add(item6);
-        list2.add(item7);
-        list2.add(item8);
-        list2.add(item9);
-
-        ArrayList<ItemDTO> list3 = new ArrayList<>();
-        list3.add(item10);
-        list3.add(item11);
-        list3.add(item12);
-        list3.add(item13);
-        list3.add(item14);
-
-        CategoryDTO category1 = new CategoryDTO(1, "Category 1", list1);
-        CategoryDTO category2 = new CategoryDTO(2, "Category 2", list2);
-        CategoryDTO category3 = new CategoryDTO(2, "Category 3", list3);
-
-        mItemList = Arrays.asList(category1, category2, category3);
+        mItemList = new ArrayList<>();
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
 
+        // TODO: This needs some rethinking, need to be able to set items dynamically
         mAdapter = new SectionAdapter(getContext(), mItemList);
-
         mAdapter.setExpandCollapseListener(new ExpandableRecyclerAdapter.ExpandCollapseListener() {
             @Override
             public void onListItemExpanded(int position) {
@@ -183,6 +139,7 @@ public class InventoryFragment extends BaseFragment {
 
             if(response.isSuccess()) {
                 mItemList = response.body();
+                // TODO: This needs some rethinking, need to be able to set items dynamically
                 mAdapter = new SectionAdapter(getContext(), mItemList);
                 mRecyclerView.setAdapter(mAdapter);
             } else {
